33a6ae6ed044968c6d11bf727201357ef0b38f22
lokua <lokua@lokua.net>
2014-08-19 04:45:15 -0500

    -auto-

--- dist/tooly-raw.js
+++ dist/tooly-raw.js
@@ -13,7 +13,7 @@ var tooly = (function() {
 
   /** @private */
   function _between(str) {
-    return new RegExp('\s*' + str + '(?!\W|\w)', 'g');
+    return new RegExp('\s*' + str + '\s*(?![\w\W])', 'g');
   }
 
   /** @private */
