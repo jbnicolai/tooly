bda0dedff8b193c1ebfda9d60716c7b1031ebe91
lokua <lokua@lokua.net>
2014-08-19 18:46:00 -0500

    -auto-

--- src/modules/xhr.js
+++ src/modules/xhr.js
@@ -1,15 +1,17 @@
 // --- begin xhr module
+// 
     /**
      * perform a get xhr request for JSON file
      * 
      * @param  {String}   jsonFile  url
-     * @param  {callback} success  function to operate on response data
-     *                             if the request is successful. If so, success
-     *                             takes a single data parameter (the response).
+     * @param  {callback} success   function to operate on response data
+     *                              if the request is successful. If so, success
+     *                              takes a single data parameter (the response).
+     * @param {Boolean}   async     defaults to true
      */
-    getJSON: function(jsonFile, success) {
+    getJSON: function(jsonFile, success, async) {
       var req = new XMLHttpRequest();
-      req.open('get', jsonFile, true);
+      req.open('get', jsonFile, async || true);
       req.reponseType = 'json';
       req.onreadystatechange = function() {
         if (req.readyState == 4) { // done
