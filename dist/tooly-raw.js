0ee775f9a9437bacce7f59a7aa4769b1098174f1
lokua <lokua@lokua.net>
2014-08-19 06:06:16 -0500

    -auto-

--- dist/tooly-raw.js
+++ dist/tooly-raw.js
@@ -306,6 +306,26 @@ var tooly = (function() {
       }
       return props;
     },    /**
+     * perform a get xhr request for JSON file
+     * 
+     * @param  {String}   jsonFile  url
+     * @param  {callback} success  function to operate on response data
+     *                             if the request is successful. If so, success
+     *                             takes a single data parameter (the response).
+     */
+    getJSON: function(jsonFile, success) {
+      var req = new XMLHttpRequest();
+      req.open('get', jsonFile, true);
+      req.reponseType = 'json';
+      req.onreadystatechange = function() {
+        if (req.readyState == 4) { // done
+          if (req.status == 200) {
+            success(req.response);
+          }
+        }
+      };
+      req.send();
+    },    /**
      * Function version of ECMAScript6 String.prototype.endsWith
      * @param  {String} str    the string to check
      * @param  {String} suffix the "endWith" we are seeking
