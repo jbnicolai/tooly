d5ec120d8a5edb88768f451df15af93b7babd72d
lokua <dev@lokua.net>
2014-09-28 03:35:24 -0500

    handler no new fix

--- src/modules/handler-constructor.js
+++ src/modules/handler-constructor.js
@@ -9,10 +9,13 @@
      * @constructor
      * @param {Object}  context   (optional) designates the owner of the `handlers` array that holds 
      *                            all callbacks. When blank the Handler instance uses its own internal
-     *                            array. If you'd like to keep track of the handlers outside of Handler,
-     *                            pass the parent owner of @param `handler` as context.
+     *                            array. If you'd like to keep track of the handlers outside of the
+     *                            instance, pass a context such that context.handlers is an array.
      */
     Handler: function(context) {
+      if (!(this instanceof Handler)) {
+        return new tooly.Handler(context);
+      }
       this.context = context || this;
       this.context.handlers = [];
       this.handlers = this.context.handlers;
