d1939ae36a759291cc9a4ac629d79e589fb4fc3a
lokua <dev@lokua.net>
2014-10-25 08:21:00 -0500

    rebuild

--- dist/tooly.js
+++ dist/tooly.js
@@ -1,5 +1,5 @@
 /*!
- * tooly - version 0.0.4 (built: 2014-10-25)
+ * tooly - version 0.0.5 (built: 2014-10-25)
  * js utility functions
  *
  * https://github.com/Lokua/tooly.git
@@ -86,7 +86,7 @@ function _sortUnique(arr) {
  * @namespace  tooly
  * @type {Object}
  */
-var tooly = { version: '0.0.4' };
+var tooly = { version: '0.0.5' };
 
 
 
@@ -315,6 +315,48 @@ tooly.Frankie.prototype.removeClass = function(klass) {
     x.className = x.className.replace(_classReg(classes), ' ').trim();
   });
   return this;
+};
+
+
+
+/**
+ * // TODO
+ * 
+ * @param  {mixed} element        the element(s) to remove from the instance's array of elements
+ *                                as well as the DOM
+ * @param  {Boolean} returnRemoved If true, the elements will be returned from the function (within)
+ *                                 a new Frankie instance 
+ *                                 (which will keep them in memory if yo store them in a reference)
+ * @return {this|Frankie}         
+ */
+tooly.Frankie.prototype.remove = function(element, returnRemoved) {
+  var ret = [], i, frank = this, len = frank.els.length, els;
+  els = _type(element, 'array') 
+    ? element
+    : _node(element)
+      ? _toArray(element)
+      : els instanceof tooly.Frankie 
+        ? element.els
+        : _type(element, 'string')
+          ? new tooly.Frankie(element, frank).els
+          : null;
+  if (els) {
+    els.forEach(function(c) { 
+      for (i = 0; i < len; i++) {
+        var p = frank.els[i];
+        if (c.parentNode === p) {
+          returnRemoved ? ret.push(p.removeChild(c)) : p.removeChild(c);
+          return; // am i needed???
+        }
+      }
+    });
+  }
+  if (returnRemoved) {
+    var f = new Frankie();
+    f.els = ret;
+    return f;
+  }
+  return frank;
 };
 
 
